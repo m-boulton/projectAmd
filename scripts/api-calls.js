702b037d1fe66a42160f467f54670b7382d4dbe3
michael <boultonmichael@gmail.com>
2021-03-09 23:38:13 -0700

    pushing to both git server and github account simultaneously

---
+++ scripts/api-calls.js
@@ -0,0 +1,76 @@
+//#TODO
+// api fetch
+// async function > await promise resolve or reject) .then function (resolve) => { resolve.map("do something")}
+
+// async function apiCall() {
+//   try {
+//     const response = await fetch(apiUrl);
+//     if (response.ok) {
+//       const data = await response.json();
+//       data.map();
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// apiCall();
+
+
+let apiUrlNav = "http://localhost:3500/amdRoutes/nav";
+// let apiUrl = "api.mboulton.com";
+
+let navContent = null;
+
+// localhost fetch nav data
+async function getApiNav() {
+  const options = 
+  const res = await fetch(apiUrlNav);
+  const data = await res.json()
+}
+
+// fetching navigation content from api source
+async function navContentLoad() {
+  console.log('retrieving nav content');
+  try {
+    if (sessionStorage.amdNavLoaded){
+      navContent = sessionStorage.amdNavContent;
+    } else if (localStorage.amdNavLoaded) {
+      async function () {
+        try {
+          const response = await fetch(apiUrl + "/nav", {
+            method : 'GET'
+          })
+          .then(function (response) {
+            return response.json().then(function(nav){
+              if (localStorage.amdBuild == nav.amdBuild) {
+                navContent = nav;
+              } else {
+              
+              }
+            })
+          })
+        } catch (error) {
+          console.log(error);
+        }
+        
+      }
+      navContent = localStorage.amdNavContent;
+    } else {
+    const response = await fetch(apiUrl)
+    if (response.ok) {
+      const data = await response.json()
+    }}
+  } catch (error) {
+    
+  }
+  console.log('nav content loaded');
+}
+
+window.onload = function productContentLoad() {
+  console.log('window has loaded');
+  async function () {
+    const result = await navContentLoad();
+
+  }
+  
+}
--- scripts/head-scripts.js
+++ scripts/head-scripts.js
@@ -1,12 +1,15 @@
 const currentPage = window.location.search;
 const currentPagePath = window.location.pathname.split(";")[0].split("/").pop();
 const page = new URLSearchParams(currentPage);
+const apiUrl = "localhost:3000";
 var pageContent = null;
+// loads the body with a template to be filled with content later
 $(function () {
   $("body").load("/pages/body-template.html");
   fillContent();
 });
 if (page.get("prod") == null) {
+  // sets the title tag in the head to the current page parameters
   if (currentPagePath == "index.html") {
     document.getElementsByClassName("title-name")[0].innerHTML =
       "AMD Technology | Welcome";
@@ -21,14 +24,16 @@ if (page.get("prod") == null) {
     pageContent = currentPagePath;
   }
 } else {
+  // reformats the url search parameters to a single word
   pageContent = page.get("prod").split("-").join(" ");
   pageContentCapitalized = pageContent.replace(/^\w/, function (c) {
     return c.toUpperCase();
   });
+  // sets the title tag in the head to the current pages parameter
   document.getElementsByClassName("title-name")[0].innerHTML =
     "AMD Technology | " + pageContentCapitalized;
 }
-
+// this fills out the html template
 setTimeout(fillContent, 500);
 function fillContent() {
   $(".header").load("/pages/dynamic-page-content/header.html");
@@ -36,3 +41,14 @@ function fillContent() {
   $(".footer").load("/pages/dynamic-page-content/footer.html");
   $(".content").load("/pages/product-content/" + pageContent + ".html");
 }
+// api call to get the content page format
+const apiFetchPageTemplate = async () => {
+  let response = await fetch(apiUrl);
+  if (response.ok) {
+    let json = await response.json();
+  } else {
+    alert("HTTP-Error: " + response.status);
+  }
+};
+
+// api call to fill the content template with current data
